112a4b9fb5aaa09e22231644273d8207dace5f52
jonghyun <puts hello,>
2015-03-24 15:21:11 +0900

    자바 실습과제

--- JavaPractice/src/org/dimigo/basic/Condition.java
+++ JavaPractice/src/org/dimigo/basic/Condition.java
@@ -5,12 +5,29 @@ public class Condition {
 	public static void main(String[] args) {
 		int distance = 10;
 		String type = "고속버스";
-		int money = 850;
+		int money = 0;
 		
-		System.out.println("거리 :" + distance);
-		System.out.println("차종 :" + type);
-		System.out.println("요금" + money);
+		
 
+		
+		switch (type){
+		case "고속버스" : 
+			money = 850 + distance/10*300;
+			if(distance %10 ==0) {money -= 300;}
+		case "경차" :
+			money = 300 + distance/10*200;
+			if(distance %10 ==0) {money -=200;}
+		default :
+			type = "그 외";
+			money = 300 + distance/10*300;
+			if(distance %10 == 0);{money -=200;}
+			
+		}		
+		System.out.println("<<고속도로 통행료 계산>>");
+		System.out.println("거리 :" + distance +"km");
+		System.out.println("차종 :" + type );
+		System.out.println("요금 :" + money + "원");
+		
 	}
 
 }
