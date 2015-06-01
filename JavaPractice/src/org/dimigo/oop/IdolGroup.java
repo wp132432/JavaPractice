9ea693652f910aa2374679732b5b0045ccdf1fbc
jonghyun <puts hello,>
2015-06-01 09:19:26 +0900

    2015년6월1일

---
+++ JavaPractice/src/org/dimigo/oop/IdolGroup.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ * |_ IdolGroup
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 6. 1.
+ * </pre>
+ * @author : 정종현
+ * @version : 1.0
+ */
+public class IdolGroup {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String[]idolGroup={"빅뱅","2ne1","걸스데이"};
+		
+		String[][]members={
+				{"GD","태양","대성","탑","승리"},
+				{"CL","산다라박","박봄","민지"},
+				{"유라","혜리","소진","민아"}
+		};
+		
+		
+		
+		for(int i=0; i<idolGroup.length; i++){
+			System.out.println("<<" + idolGroup[i]+"멤버" +">>");
+			for(int j=0; j<members[i].length; j++) {
+			System.out.println(members[i][j]);
+			}
+			}
+		}
+
+	}
+
+
